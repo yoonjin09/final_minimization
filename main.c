6b5ec7cb125ecbe7cf7bb3c55efb0761a334744d
yoonjin09 <63450174+yoonjin09@users.noreply.github.com>
2020-06-02 22:54:56 +0900

    Update main.c

--- main.c
+++ main.c
@@ -4,12 +4,13 @@
 #include "arraymin.h"
 
 int main(){
-  char all[50]="";
+    char all[50]="";
   char dontcare[50]="";
-  Variable list[40] ; 
+  Variable list[40] ;
   Variable Epilist[2048];
   Variable tokenlist[30];
-  Variable finallist[20]; 
+  Variable finallist[20];
+  printf("array");
   int count=0; //minterm 개수
   int varcount=0; //변수 개수
   int i= 0;
@@ -25,16 +26,14 @@ int main(){
   printf("minterm 과 dont care 개수: \n");
   scanf("%d %d", &mintermcount, &dontcarecount);
   getchar();
-  printf("minterm 리스트: \n");
   scanf("%[^\n]s", all);
   if(dontcarecount!=0){
     getchar();
-    printf("don't care 리스트: \n");
     scanf("%[^\n]s", dontcare);
     strcat(all," ");
     strcat(all,dontcare);
   }
-  
+
   token=strtok(all," ");
   while(token!=NULL){
     list[i].num = atoi(token);
@@ -43,27 +42,44 @@ int main(){
       ipisdontcare++;
     }
     else list[i].isdontcare =0;
-    
-    list[i].numeric[0]=list[i].num; 
+
+    list[i].numeric[0]=list[i].num;
     list[i].numericcount++;
+    //printf("%d\n",atoi(token));
+    printf("%d \n",list[i].num);
     i++;
     token=strtok( NULL," ");
     count++;
   }
 
-        
+    printf("\n Debug 1\n"); //입력받은 것을 doncare 인지 minterm으로 받은건지 구분이 끝났다.
+
+
     while(a<count){
     int tok=list[a].num;
+    //printf("%d \n",tok);
     for(int j = (varcount-1); j> -1 ; j--){
-        if(tok == 0) list[a].binary[j]=0; 
+        if(tok == 0) list[a].binary[j]=0;
         else list[a].binary[j]=tok%2;
         tok=tok/2;
     }
     a++;
-    } // 이진수로 변환 
+    } // 이진수로 변환
     a=0;
 
-    
+    printf("\n Debug 2\n"); // 이진수로 바꿔서 binary에 집어 넣는다.
+
+
+    for(int a=0; a<count; a++){
+        int count=0;
+        for(int j=0; j< varcount; j++){
+        count++;
+        printf("%d ",list[a].binary[j]);
+        if(count%varcount==0)printf("  %d\n",list[a].epi);
+        }
+    }
+
+
     while(a< count){
         int samecount=0; //같은 자리수의 값이 같은 개수 확인용
         int confirmsamecount=count; //모든 값에 대해 samecount가 3이하이면 그것도 추가
@@ -78,8 +94,8 @@ int main(){
                     if(list[a].binary[b] == list[A].binary[b]){
                         Epilist[epicount].binary[b] = list[a].binary[b]; //원래는 '-'를 넣어서 표현했지만 0,1 이 아닌 수 를 집어 넣어준 것
                     }
-                    else Epilist[epicount].binary[b]= 2;     
-                }   
+                    else Epilist[epicount].binary[b]= 2;
+                }
                 confirmsamecount--;
                 epicount++;
                 list[a].epi=1;
@@ -89,18 +105,24 @@ int main(){
             Epilist[epicount]=list[a];
             epicount++;
             list[a].epi=0;
-        } 
+        }
         a++;
     }
-    
-    
+
+
     a=0;
+
+    printf("\n Debug 3\n"); //각 자리수를 한 번씩 다 비교. 만약 같은 것이 없다면 그것도 epilist로 들어간다
+
+    printBinary(epicount, varcount, Epilist);
+    printNumeric(epicount, varcount, Epilist);
 
     int epilistcount=0;
     int nextepicount=0;
-    
-    
+
+
     while(epilistcount!=epicount){
+        printf("epicount: %d\n",epicount);
         epilistcount=epicount;
         int bconfirm=0;
         for(int a=nextepicount; a< epilistcount-1; a++){
@@ -115,23 +137,22 @@ int main(){
                         if(Epilist[a].binary[b] == Epilist[A].binary[b]){
                             Epilist[epicount].binary[b] = Epilist[a].binary[b]; //원래는 '-'를 넣어서 표현했지만 0,1 이 아닌 수 를 집어 넣어준 것
                         }
-                        else Epilist[epicount].binary[b]= 2;     
+                        else Epilist[epicount].binary[b]= 2;
                     }
                     int episamecount=0; // epi에서 새로운거 추가할 때 그 전에 같은 것이 있으면 1로 바뀐다.
-                    
+
                     for(int x=0; x<epilistcount; x++){
                         samecount=0;
-                        if(Epilist[x].epi==0){
                             for(int b=0; b<varcount; b++){
-                                if(Epilist[epicount].binary[b] == Epilist[x].binary[b]) samecount++;        
+                                if(Epilist[epicount].binary[b] == Epilist[x].binary[b]) samecount++;
                             }
                             if(samecount == varcount){
-                                epicount--;
+                               // epicount--;
                                 //continue;
                                 episamecount=1;
                                 break;
                             }
-                        }
+
                         samecount=0;
                     }
                     //
@@ -146,7 +167,7 @@ int main(){
                                 Epilist[epicount].numericcount++;
                             }
                         }
-                        
+
                         for(int nucount = 0; nucount<Epilist[A].numericcount;nucount++){
                             int kcount=0;
                             for( int k=0; k< Epilist[epicount].numericcount; k++){
@@ -156,19 +177,35 @@ int main(){
                                 Epilist[epicount].numeric[Epilist[epicount].numericcount]= Epilist[A].numeric[nucount];
                                 Epilist[epicount].numericcount++;
                             }
-                        } 
+                        }
+                        /*
+                        for(int nucount = 0; nucount<Epilist[A]->numericcount;nucount++){
+                            Epilist[epicount]->numeric[Epilist[epicount]->numericcount]= Epilist[A]->numeric[nucount];
+                            Epilist[epicount]->numericcount++;
+                        }
+                        */
+
+                        //Epilist[epicount]->numeric[(Epilist[epicount]->numericcount)]= -1;
                         epicount++;
                         Epilist[a].epi=1;
                         Epilist[A].epi=1;
                     }
-                    episamecount=0;   
+                    episamecount=0;
                 }
                 samecount=0;
             }
         }
         nextepicount=epilistcount;
     }
-    
+
+
+    printf("\n Debug 4\n"); //각 자리수를 한 번씩 다 비교. 만약 같은 것이 없다면 그것도 epilist로 들어간다
+
+    printBinary(epicount, varcount, Epilist);
+    printNumeric(epicount, varcount, Epilist);
+
+
+
     int tokencount=0;
     for(int a=epicount-1; a>-1 ; a--){
         int samecount=0; //같은 자리수의 값이 같은 개수 확인용
@@ -178,10 +215,11 @@ int main(){
         }
         for(int x=0; x<tokencount-1; x++){
             for(int b=0; b<varcount; b++){
-                if(tokenlist[tokencount-1].binary[b] == tokenlist[x].binary[b]) samecount++;        
+                if(tokenlist[tokencount-1].binary[b] == tokenlist[x].binary[b]) samecount++;
             }
             if(samecount == varcount){
                 tokencount--;
+                //continue;
                 break;
             }
             samecount=0;
@@ -189,12 +227,19 @@ int main(){
     }
 
 
+    printf("\n Debug 5\n"); //epi가 0인 것만 tokenlist에 집어 넣는다.
+
+    printBinary(tokencount, varcount, tokenlist);
+    printNumeric(tokencount, varcount, tokenlist);
+
+
+
     int col=tokencount;
     int row=mintermcount;
     int to_darray[col][row];
 
     int **twod_array;
-    twod_array=(int **)malloc(tokencount * sizeof(int*));  
+    twod_array=(int **)malloc(tokencount * sizeof(int*));
 
     for(int z=0; z<tokencount ; z++){
         for(int y=0; y<mintermcount; y++){
@@ -216,7 +261,20 @@ int main(){
         for(int y=0; y<tokencount; y++){
             if(twod_array[y][z]!= 1) twod_array[y][z]=0;
         }
+        printf("\n");
     }
+
+
+    for(int z=0; z<mintermcount; z++){
+        printf("%d",list[z].num);
+    }
+
+
+    printf("\n");
+    printf("-------------------\n");
+    print2darray(tokencount, mintermcount, twod_array);
+    printf("\n");
+    printf("-------------------\n");
 
     int finalcount=0; //finallist에 들어갈 minterm이 몇 개인지 확인
     int exitcount=0;
@@ -241,11 +299,14 @@ int main(){
                             }
                             twod_array[z][Y]=0;
                         }
-                    } 
-                    twod_array[z][y] = 0; 
-                } 
+                    }
+                    twod_array[z][y] = 0;
+                }
             }
         }
+
+    printf("Step 1: \n");
+    print2darray(tokencount, mintermcount, twod_array);
 
     for(int y=0; y<tokencount-1; y++){//'-1'을 하는 이유: 마지막에 있는 것은 비교할게 없으니 마지막에서 두번째와 마지막을 비교하는 것이 맞다.
         for(int Y=y+1; Y<tokencount;Y++){
@@ -267,24 +328,27 @@ int main(){
                     for(int z=0; z<mintermcount; z++){
                         twod_array[y][z]= 0;
                     }
-                } 
+                }
                 rowcount=0;
             }
             if(rowcount > 0 && onecount == rowcount) {
                 for(int z=0; z<mintermcount; z++){
                     twod_array[y][z]= 0;
-                } 
+                }
             }
             else if(rowcount > 0 && onecountnext == rowcount) {
                 for(int z=0; z<mintermcount; z++){
                     twod_array[Y][z]= 0;
-                } 
+                }
             }
+            printf("y: %d rowcount: %d onecount: %d onecountnext: %d\n", y,rowcount,onecount,onecountnext);
         }
     }
-    
 
 
+
+    printf("Step 2: \n");
+    print2darray(tokencount, mintermcount, twod_array);
 
     for(int y=0; y<mintermcount-1; y++){//'-1'을 하는 이유: 마지막에 있는 것은 비교할게 없으니 마지막에서 두번째와 마지막을 비교하는 것이 맞다.
         for(int Y=y+1; Y<mintermcount;Y++){
@@ -295,35 +359,51 @@ int main(){
                 if(twod_array[z][y] == 1 && twod_array[z][Y] == 1) colcount++;
                 if(twod_array[z][y] == 1) onecount++;
                 if(twod_array[z][Y] == 1) onecountnext++;
-            } 
+            }
             if(colcount > 0 && onecount == colcount) {
                 for(int z=0; z<tokencount; z++){
                     twod_array[z][Y]= 0;
-                } 
+                }
             }
             else if(colcount > 0 && onecountnext == colcount) {
                 for(int z=0; z<tokencount; z++){
                     twod_array[z][y]= 0;
-                } 
+                }
             }
-        }   
+            printf("y: %d colcount: %d onecount: %d onecountnext: %d\n", y,colcount,onecount,onecountnext);
+        }
     }
-    
 
+
+    printf("Step 3: \n");
+    print2darray(tokencount, mintermcount, twod_array);
+
+    for(int a=0; a<finalcount; a++){ //finallist에 들어갔으면 보여주는 것
+        int count=0;
+        for(int j=0; j< varcount; j++){
+            count++;
+            printf("%d ",finallist[a].binary[j]);
+            if(count%varcount==0) printf("\n");
+        }
+    }
     int zerocount =0;
     for(int z=0; z<tokencount; z++){
         for(int y=0; y<mintermcount; y++){
-            if(twod_array[z][y] == 0) zerocount++; 
+            if(twod_array[z][y] == 0) zerocount++;
         }
     }
-    
+
     if(zerocount == (tokencount*mintermcount)) break;
     }
 
-printf("\n");
+for(int z=0; z<count;z++){
+  printf("%d %d\n",z,list[z].isdontcare);
+}
+
+
 for(int a=0; a<finalcount; a++){
     for(int b=0; b< varcount; b++){
-        
+
         switch(finallist[a].binary[b]){
             case 0 :
                 printf("x%d'",b+1);
@@ -335,12 +415,12 @@ for(int a=0; a<finalcount; a++){
                 break;
 
         }
-        
+
     }
     if(a != finalcount -1) printf(" + ");
 }
 
-printf("\n");
+
 
     return 0;
 
